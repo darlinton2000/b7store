386580bfbe29dda8adfaa2d57082d9b78e753866
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-10-04 23:10:23 -0300

    Registro e Login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
