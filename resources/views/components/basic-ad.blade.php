2383d8faf586f110dfeb3221b7b8fbe9e1254c92
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2024-01-13 10:35:40 -0300

    Criando pill meus anuncios

--- resources/views/components/basic-ad.blade.php
+++ resources/views/components/basic-ad.blade.php
@@ -1,4 +1,25 @@
+<style>
+    .my-ad-item {
+        position: relative;
+    }
+    .pill {
+        padding: 5px 10px;
+        background-color: teal;
+        color: white;
+        border-radius: 5px;
+    }
+    .my-ad-pill {
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        font-size: 10px;
+    }
+</style>
+
 <div class="my-ad-item">
+    @if(empty($canEdit) && $ad->user_id === Auth::user()->id)
+        <span class="pill my-ad-pill">Meu Anúncio</span>
+    @endif
     <div class="ad-image-area">
         @if(!empty($canEdit))
             <div class="ad-buttons">
