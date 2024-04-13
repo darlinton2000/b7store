72c166f21536f1d5e447fc8d404895808ecf0929
darlinton2000 <darlinton2000@gmail.com>
2024-04-12 22:24:30 -0300

    refactor: Refatorando hero section

--- app/View/Components/hero.php
+++ app/View/Components/hero.php
@@ -2,6 +2,8 @@
 
 namespace App\View\Components;
 
+use App\Models\Category;
+use App\Models\State;
 use Illuminate\View\Component;
 
 class hero extends Component
@@ -16,42 +18,8 @@ class hero extends Component
      */
     public function __construct()
     {
-        // Carregando todos os estados do Brasil
-        $this->states = [
-            ['value' => 'AC', 'name' => 'Acre'],
-            ['value' => 'AL', 'name' => 'Alagoas'],
-            ['value' => 'AP', 'name' => 'Amapá'],
-            ['value' => 'AM', 'name' => 'Amazonas'],
-            ['value' => 'BA', 'name' => 'Bahia'],
-            ['value' => 'CE', 'name' => 'Ceará'],
-            ['value' => 'DF', 'name' => 'Distrito Federal'],
-            ['value' => 'ES', 'name' => 'Espírito Santo'],
-            ['value' => 'GO', 'name' => 'Goiás'],
-            ['value' => 'MA', 'name' => 'Maranhão'],
-            ['value' => 'MT', 'name' => 'Mato Grosso'],
-            ['value' => 'MS', 'name' => 'Mato Grosso do Sul'],
-            ['value' => 'MG', 'name' => 'Minas Gerais'],
-            ['value' => 'PA', 'name' => 'Pará'],
-            ['value' => 'PB', 'name' => 'Paraíba'],
-            ['value' => 'PR', 'name' => 'Paraná'],
-            ['value' => 'PE', 'name' => 'Pernambuco'],
-            ['value' => 'PI', 'name' => 'Piauí'],
-            ['value' => 'RJ', 'name' => 'Rio de Janeiro'],
-            ['value' => 'RN', 'name' => 'Rio Grande do Norte'],
-            ['value' => 'RS', 'name' => 'Rio Grande do Sul'],
-            ['value' => 'RO', 'name' => 'Rondônia'],
-            ['value' => 'RR', 'name' => 'Roraima'],
-            ['value' => 'SC', 'name' => 'Santa Catarina'],
-            ['value' => 'SP', 'name' => 'São Paulo'],
-            ['value' => 'SE', 'name' => 'Sergipe'],
-            ['value' => 'TO', 'name' => 'Tocantins']
-        ];
-
-        // Carregando categorias
-        $this->categories = [
-            ['value' => 'categoria1', 'name' => 'Categoria 1'],
-            ['value' => 'categoria2', 'name' => 'Categoria 2'],
-        ];
+        $this->states = State::all();
+        $this->categories = Category::all();
     }
 
     /**
